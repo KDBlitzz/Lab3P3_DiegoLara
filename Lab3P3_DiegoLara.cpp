bc230a2abc9bdd2215dd4cfe4622031869b2d0a8
Diego Lara <diegoalara2005@gmail.com>
2024-02-02 17:46:17 -0600

    Quinto y ultimo commit

--- Lab3P3_DiegoLara.cpp
+++ Lab3P3_DiegoLara.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 using namespace std;
+#include<cstdlib>
+#include<ctime>
 #include <locale>
 int generarNumRandom(int minimo, int maximo) { // metodo para generar numeros al azar de 1 a 20
 	int random;
 	random = 0;
 	random = minimo + (rand() % maximo);
-	return random;
+	return random; 
 }
-void ordenadoAscendentemente() {
-
+void ordenadoAscendentemente(int arreglo[], int longitud) { // metodo para ordenar ascendentemente el ejercicio 2
+	int contador, bandera;
+	for (int i = 1; i < longitud; i++)
+	{
+		contador = arreglo[i];
+		for (bandera = i - 1; bandera >= 0 && arreglo[bandera] > contador; bandera--)
+		{
+			arreglo[bandera + 1] = arreglo[bandera];
+		}
+		arreglo[bandera + 1] = contador;
+	}
 }
 void ejercicio_1() {
 	int numero = 0;
 	int longitud = 0;
 	cout << "Ingrese el size del arreglo: " << endl;
 	cin >> longitud;
-	while (longitud < 5) {
+	while (longitud < 6) { // Validar que sea mayor a 5
 		cout << "Favor ingresar un numero mayor a 5" << endl; cin >> longitud;
 	}
-	int* arreglo = new int[longitud];
+	int* arreglo = new int[longitud]; // los arreglos a usar
 	int* arregloVacio = new int[longitud];
 	cout << "Ingrese los numeros del arreglo: " << endl;
 	for (int i = 0; i < longitud; i++)
 	{
 		cin >> numero;
-		while (numero < 1 || numero > 15) {
+		while (numero < 1 || numero > 15) { // validar que sea un numero entre 1 y 15
 			cout << "Favor ingrese el numero entre 1 y 15" << endl; cin >> numero;
 		}
 		arreglo[i] = numero;
 	}
 	cout << "Arreglo antes: ";
 	cout << "[";
-	for (int i = 0; i < longitud; i++)
+	for (int i = 0; i < longitud; i++) // imprimir el arreglo antes
 	{
 		if (i != longitud - 1)
 		{
@@ -44,22 +55,22 @@ void ejercicio_1() {
 	cout << "]" << endl;
 	int comprobar = 0;
 	int contador = 0;
-	for (int i = 0; i < longitud; i++)
+	for (int i = 0; i < longitud; i++) // un for que recibe los numeros que no son 7
 	{
 		if (arreglo[i] != 7)
 		{
 			arregloVacio[contador] = arreglo[i];
 			contador++;
 		}
-		else {
+		else { // comprobar que allan 7s
 			comprobar = 1;
 		}
 	}
-	for (int i = contador; i < longitud; i++)
+	for (int i = contador; i < longitud; i++) // for que rellena los espacios vacios con 7
 	{
 		arregloVacio[i] = 7;
 	}
-	if (comprobar == 0)
+	if (comprobar == 0) // if para ver si no tiene 7s y si si tenia pues imprimirlos en el arreglo despues
 	{
 		cout << "Vale más que no está el 7, ¡porque no es óptimo!" << endl;
 	}
@@ -78,21 +89,25 @@ void ejercicio_1() {
 		}
 		cout << "]" << endl;
 	}
-	delete[] arreglo;
-	delete[] arregloVacio;
+	
+	delete[] arreglo; 
+	delete[] arregloVacio; // Borrar la memoria
 }
 void ejercicio_2() {
 	int longitud = 0;
 	int numeroObjetivo = 0;
 	cout << "Ingrese el size del arreglo: "; cin >> longitud;
-	int* arreglo = new int[longitud];
-	for (int i = 0; i < longitud; i++)
+	while (longitud < 5) {
+		cout << "Ingrese un size mayor o igual que 5" << endl; cin >> longitud;
+	}
+	int* arreglo = new int[longitud]; // puntero usado
+	for (int i = 0; i < longitud; i++) // for para generar el random adentro del arreglo
 	{
 		arreglo[i] = generarNumRandom(1, 20);
 	}
 	cout << "Arreglo random generado: ";
 	cout << "[";
-	for (int i = 0; i < longitud; i++)
+	for (int i = 0; i < longitud; i++) // imprimir el arreglo
 	{
 		if (i != longitud - 1)
 		{
@@ -103,10 +118,52 @@ void ejercicio_2() {
 		}
 	}
 	cout << "]" << endl;
-	cout << "Ingrese el número objetivo: " << endl; cin >> numeroObjetivo;
-
+	cout << "Ingrese el número objetivo (entre 1 y 20): " << endl; cin >> numeroObjetivo;
+	while (numeroObjetivo < 1 || numeroObjetivo > 20) { // validar que sea entre 1 y 20
+		cout << "Ingrese un número entre 1 y 20" << endl; cin >> numeroObjetivo; 
+	}
+	ordenadoAscendentemente(arreglo, longitud); // metodo de ordenamiento
+	cout << "Arreglo ordenado: ";
+	cout << "[";
+	for (int i = 0; i < longitud; i++) // imprimir el arreglo ordenado
+	{
+		if (i != longitud - 1)
+		{
+			cout << arreglo[i] << ",";
+		}
+		else {
+			cout << arreglo[i];
+		}
+	}
+	cout << "]" << endl;
+	int numeroEncontrado = 0; // bandera utilizada para ver si el numero esta o no
+	for (int i = 0; i < longitud; i++) // verificar si esta el numero que eligio el usuario
+	{
+		if (arreglo[i] == numeroObjetivo)
+		{
+			cout << "El número " << numeroObjetivo << " se encuentra en la posición " << i << " del arreglo." << endl;
+			numeroEncontrado = 1;
+			break;
+		}
+	} 
+	if (numeroEncontrado == 0)
+	{
+		int i;
+		for (i = 0; i < longitud; i++)
+		{
+			if (arreglo[i] > numeroObjetivo) // validar en que posicion deberia estar el numero
+			{
+				cout << "El número " << numeroObjetivo << " debería ir en la posición " << i << " del arreglo." << endl;
+				break;
+			}
+		}
+		if (arreglo[i] < numeroObjetivo) { // validar si el numero no esta en el arreglo y es mayor a los demas
+			cout << "El número " << numeroObjetivo << " debería ir en la posición " << i - 1 << " del arreglo." << endl;
+		}
+	}
+	delete[] arreglo;
 }
-void menu() {
+void menu() { // menu
 	int option = 0;
 	bool menu = true;
 	while (menu) {
@@ -125,12 +182,13 @@ void menu() {
 			menu = false;
 			break;
 		default:
-			cout << "Opcion Invalida";
+			cout << "Opcion Invalida" << endl;
 		}
 	}
 }
 int main()
 {
 	setlocale(LC_ALL, "spanish");
+	srand(time(NULL));
 	menu();
 }
